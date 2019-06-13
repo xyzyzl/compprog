d477b31c050fcf3d1df75ace98fff5f1bcfc2479
Albert Ye <albert.ye.728@gmail.com>
2019-06-12 22:20:17 -0700

    Test

---
+++ probs/uva/cliondrunk/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+	freopen("moo.txt", "r", stdin);
+	int n=100000;
+	cin >> n;
+	cout << n;
+	return 0;
+}
---
+++ probs/uva/cloneof11235/cmake-build-debug/CMakeFiles/cloneof11235.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cloneof11235.dir/main.cpp.o"
+  "cloneof11235.pdb"
+  "cloneof11235"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cloneof11235.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
