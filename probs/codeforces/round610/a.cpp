4e9cc3e7d2e2008f094f0588796acc266b94cedb
Albert Ye <41131127+xyzyzl@users.noreply.github.com>
2019-12-26 07:58:10 +0800

    github not banned in china!

---
+++ probs/codeforces/round610/a.cpp
@@ -0,0 +1,77 @@
+// Time:
+// Algorithms: 
+
+#include <bits/stdc++.h>
+
+#pragma GCC optimize("O3")
+
+using namespace std;
+
+#define MAXN 1000005
+
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define FORR(j, i, n) for (int i = j; i < n; i++)
+#define FORD(i, n) for (int i = n - 1; i >= 0; i--)
+#define FOBIR(i, n) for (int i = 1; i <= n; i++)
+#define FOBIRD(i, n) for (int i = n; i >= 1; i--)
+#define pb push_back
+#define mp make_pair
+#define endl '\n'
+#define DUEHOANG                  \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);                \
+    cout.tie(NULL)
+
+#define fileio(in, out)      \
+    freopen(in, "r", stdin); \
+    freopen(out, "w", stdout);
+
+#define ll long long
+#define INF (1e9*1)+5
+
+typedef set<int> si;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef pair<int, pii> iii;
+typedef vector<pii> vii;
+typedef vector<iii> viii;
+typedef priority_queue<int> pqi;
+typedef stack<int> sti;
+typedef queue<int> qi;
+typedef deque<int> di;
+typedef map<int, int> mii;
+#define f first
+#define s second
+
+#define LEFT(x) 2 * x
+#define RIGHT(x) 2 * x + 1
+
+#define bitinc(x) x&-x
+
+const int MAX(int &a, int b)
+{
+    return a = max(a, b); 
+}
+const int MIN(int &a, int b)
+{
+    return a = min(a, b); 
+}
+
+int main()
+{
+    DUEHOANG;
+    int t = 1;
+    cin >> t; // uncomment if it's multitest
+    while(t--)
+    {
+        int a, b, c, r;
+        cin >> a >> b >> c >> r;
+        int x, y;
+        x = min(a,b);
+        y = max(a,b);
+        int m = min((c+r), y);
+        int n = max((c-r), x);
+        cout << ((y-x)-(max(m-n, 0))) << endl;
+    }
+    
+}
