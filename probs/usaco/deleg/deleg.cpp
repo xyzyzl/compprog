a7e911c64c6febab9fd165495ec37288e213d165
Albert Ye <albert.ye.728@gmail.com>
2020-03-13 18:22:41 -0700

    got the first two usaco problems, deleg wrong

--- probs/usaco/deleg/deleg.cpp
+++ probs/usaco/deleg/deleg.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-#define MAXN 1000005
+#define MAXN 100005
 
 #define FOR(i, n) for (int i = 0; i < n; i++)
 #define FORR(j, i, n) for (int i = j; i < n; i++)
@@ -58,13 +58,74 @@ const int MIN(int &a, int b)
     return a = min(a, b); 
 }
 
+int n, from[MAXN];
+vi adj[MAXN], thing[MAXN];
+
+void dfs(int v, int p)
+{
+	from[v] = 1;
+	for(int x : adj[v])
+	{
+		if(x == p) continue;
+		dfs(x, v);
+		from[v] += from[x];
+		thing[v].pb(from[x]);
+	}
+	if(from[v] != n) thing[v].pb(n-from[v]);
+}
+
+int cur[MAXN];
+bool test(int x)
+{
+	if((n-1)%x != 0) return 0;
+	// cout << x << endl;
+	FOR(i, n)
+	{
+		int y = 0;
+		// pairing things up
+		for(int len : thing[i])
+		{
+			int z = len%x;
+			if(!z) continue;
+			if(cur[x-z])
+			{
+				cur[x-z]--;
+				y--;
+			} else
+			{
+				cur[z]++;
+				y++;
+			}
+		}
+		if(y) return 0; // pairing failed, there is some
+		// number that cannot form a path of length k
+	}
+	return 1;
+}
+
 int main()
 {
+	fileio("deleg.in", "deleg.out");
     int t = 1;
     // cin >> t; // uncomment if it's multitest
     while(t--)
     {
-    
+		cin >> n;
+		FOR(i, n-1)
+		{
+			int a, b;
+			cin >> a >> b;
+			--a; --b;
+			adj[a].pb(b);
+			adj[b].pb(a);
+		}
+		dfs(0, -1);
+		FOBIR(i, n-1)
+		{
+			cout << test(i);
+			// have some method to find if it's possible
+		}
+		cout << endl;
     }
     
 }
---
+++ probs/usaco/help/help.cpp
@@ -0,0 +1,116 @@
+// Time:
+// Algorithms: 
+
+#include <bits/stdc++.h>
+
+#pragma GCC optimize("O3")
+
+using namespace std;
+
+#define MAXN 1000005
+
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define FORR(j, i, n) for (int i = j; i < n; i++)
+#define FORD(i, n) for (int i = n - 1; i >= 0; i--)
+#define FOBIR(i, n) for (int i = 1; i <= n; i++)
+#define FOBIRD(i, n) for (int i = n; i >= 1; i--)
+#define pb push_back
+#define mp make_pair
+#define endl '\n'
+#define DUEHOANG                  \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);                \
+    cout.tie(NULL)
+
+#define fileio(in, out)      \
+    freopen(in, "r", stdin); \
+    freopen(out, "w", stdout);
+
+#define ll long long
+#define INF (1e9*1)+5
+
+typedef set<int> si;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef pair<int, pii> iii;
+typedef vector<pii> vii;
+typedef vector<iii> viii;
+typedef priority_queue<int> pqi;
+typedef stack<int> sti;
+typedef queue<int> qi;
+typedef deque<int> di;
+typedef map<int, int> mii;
+#define f first
+#define s second
+
+#define LEFT(x) 2 * x
+#define RIGHT(x) 2 * x + 1
+
+#define bitinc(x) x&-x
+
+const int MAX(int &a, int b)
+{
+    return a = max(a, b); 
+}
+const int MIN(int &a, int b)
+{
+    return a = min(a, b); 
+}
+
+#define int ll
+#define MOD 1e9+7
+
+// due to Benjamin Qi
+// this I believe is now deleted; last year I was informed by a classmate about this snippet
+
+namespace modOp
+{
+    int ad(int a, int b, int mod = MOD) { return (a+b)%mod; }
+    int sub(int a, int b, int mod = MOD) { return (a-b+mod)%mod; }
+    int mul(int a, int b, int mod = MOD) { return (ll)a*b%mod; }
+ 
+    int AD(int& a, int b, int mod = MOD) { return a = ad(a,b,mod); }
+    int SUB(int& a, int b, int mod = MOD) { return a = sub(a,b,mod); }
+    int MUL(int& a, int b, int mod = MOD) { return a = mul(a,b,mod); }
+ 
+    int po (int b, int p, int mod = MOD) { return !p?1:mul(po(mul(b,b,mod),p/2,mod),p&1?b:1,mod); }
+    int inv (int b, int mod = MOD) { return po(b,mod-2,mod); }
+ 
+    int invGeneral(int a, int b) { // 0 < a < b, gcd(a,b) = 1
+        if (a == 0) return b == 1 ? 0 : -1;
+        int x = invGeneral(b%a,a);
+        return x == -1 ? -1 : ((1-(ll)b*x)/a+b)%b;
+    }
+}
+ 
+using namespace modOp; // benq is lying when saying that no one can help me
+// because he just helped me a great deal :blobheart:
+
+int n, e[MAXN*2], d[MAXN*2], twotothe[MAXN*2];
+pii ln[MAXN];
+signed main()
+{
+    fileio("help.in", "help.out");
+    int t = 1;
+    // cin >> t; // uncomment if it's multitest
+    while(t--)
+    {
+        cin >> n;
+        FOR(i, n)
+        {
+            cin >> ln[i].f >> ln[i].s;
+            e[ln[i].f] = 1;
+            e[ln[i].s] = -1;
+        }
+        FOBIR(i, 2*n) e[i] += e[i-1];
+        // sweepline
+        // FOR(i, n) cerr << e[i] << endl;
+        int res = 0;
+        FOR(i, n)
+        {
+			AD(res, po(2, n-1-e[ln[i].f-1]));
+        }
+        cout << res << endl;
+    }
+    
+}
