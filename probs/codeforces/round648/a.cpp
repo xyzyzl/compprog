6056cf8ea273e8aba9428519b97a3ea516b13f87
Albert Ye <albert.ye.728@gmail.com>
2020-06-09 20:46:29 -0700

    failed round... still need to solve DEF

---
+++ probs/codeforces/round648/a.cpp
@@ -0,0 +1,109 @@
+// Time:
+// Algorithms: 
+
+#include <bits/stdc++.h>
+
+#pragma GCC optimize("O3")
+
+using namespace std;
+
+#define MAXN 1000005
+
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define FORR(j, i, n) for (int i = j; i < n; i++)
+#define FORD(i, n) for (int i = n - 1; i >= 0; i--)
+#define F1R(i, n) for (int i = 1; i <= n; i++)
+#define F1RD(i, n) for (int i = n; i >= 1; i--)
+#define pb push_back
+#define mp make_pair
+#define endl '\n'
+#define DUEHOANG                  \
+	ios_base::sync_with_stdio(0); \
+	cin.tie(NULL);                \
+	cout.tie(NULL)
+
+#define fileio(in, out)      \
+	freopen(in, "r", stdin); \
+	freopen(out, "w", stdout);
+
+#define ll long long
+#define MOD (1e9*1)+7
+#define MOD2 998244353
+#define INF (1e9*1)+5
+
+typedef set<int> si;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef pair<int, pii> iii;
+typedef vector<pii> vii;
+typedef vector<iii> viii;
+typedef priority_queue<int> pqi;
+typedef stack<int> sti;
+typedef queue<int> qi;
+typedef deque<int> di;
+typedef map<int, int> mii;
+#define f first
+#define s second
+
+#define LEFT(x) 2 * x
+#define RIGHT(x) 2 * x + 1
+
+#define bitinc(x) x&-x
+
+const int MAX(int &a, int b)
+{
+	return a = max(a, b); 
+}
+const int MIN(int &a, int b)
+{
+	return a = min(a, b); 
+}
+
+int n, m, ct;
+vi ur, uc;
+void read()
+{
+	cin >> n >> m;
+	ur.clear();
+	ur.resize(n);
+	uc.clear();
+	uc.resize(m);
+	ct=0;
+	FOR(i, n) FOR(j, m)
+	{
+		int x;
+		cin >> x;
+		if(x == 1)
+		{
+			ur[i] = 1;
+			uc[j] = 1;
+		}
+	}
+}
+
+void solve()
+{
+	int a = 0, b = 0;
+	FOR(i, n)
+	{
+		if(!ur[i]) a++;
+	}
+	FOR(i, m)
+	{
+		if(!uc[i]) b++;
+	}
+	cout << (min(a, b) % 2 ? "Ashish" : "Vivek") << endl;
+}
+
+int main()
+{
+	DUEHOANG;
+	int t = 1;
+	cin >> t; // uncomment if it's multitest
+	while(t--)
+	{
+		read();
+		solve();
+	}
+	
+}
