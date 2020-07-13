71587a0761607a66a966bffc8b928d8f9a8397d4
Albert Ye <albert.ye.728@gmail.com>
2020-07-13 16:13:26 -0700

    finished some pbds problem (treap) updated template with one ds from pbds

--- probs/codeforces/round567/d/d.cpp
+++ probs/codeforces/round567/d/d.cpp
@@ -9,8 +9,9 @@
 #pragma GCC optimize("O3")
 
 using namespace std;
+using namespace __gnu_pbds;
 
-#define MAXN 1000005
+#define MAXN 500005
 
 #define FOR(i, n) for (int i = 0; i < n; i++)
 #define FORR(j, i, n) for (int i = j; i < n; i++)
@@ -30,6 +31,7 @@ using namespace std;
 	freopen(out, "w", stdout);
 
 #define ll long long
+#define int ll
 #define MOD (1e9*1)+7
 #define MOD2 998244353
 #define INF (1e9*1)+5
@@ -45,8 +47,7 @@ typedef stack<int> sti;
 typedef queue<int> qi;
 typedef deque<int> di;
 typedef map<int, int> mii;
-// ordered_set
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> os;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 #define f first
 #define s second
 
@@ -64,15 +65,63 @@ const int MIN(int &a, int b)
 	return a = min(a, b); 
 }
 
+int n, m, mf, q, a[MAXN], f[MAXN];
+vi frq[MAXN];
+ll qry[MAXN], srt[MAXN];
+ordered_set os;
+map<ll, ll> ret;
 void read()
 {
+	cin >> n >> m >> q;
+	FOR(i, n) cin >> a[i]; 
+	FOR(i, q)
+	{
+		cin >> qry[i];
+		qry[i] -= n;
+		srt[i] = qry[i];
+	}
+	sort(srt, srt+q);
+	sort(a, a+n);
+	FOR(i, n)
+	{
+		f[a[i]]++;
+	}
+	F1R(i, m)
+	{
+		frq[f[i]].push_back(i);
+		MAX(mf, f[i]);
+	}
 }
 
 void solve()
 {
+	int cur = 0, ind = 0;
+	F1R(h, mf)
+	{
+		int pre = cur;
+		for(int i : frq[h-1])
+		{
+			os.insert(i);
+		}
+		cur += os.size();
+		while(srt[ind] != 0 && srt[ind] <= cur)
+		{
+			ret[srt[ind]] = *os.find_by_order(srt[ind] - pre - 1);
+			ind++;
+		}
+	}
+	FOR(i, q)
+	{
+		if(!ret[qry[i]])
+		{
+			cout << (qry[i] - cur - 1)%m + 1 << endl;
+			continue;
+		}
+		cout << ret[qry[i]] << endl;
+	}
 }
 
-int main()
+signed main()
 {
 	DUEHOANG;
 	int t = 1;
@@ -81,6 +130,6 @@ int main()
 	{
 		read();
 		solve();
-	}
-	
+	}	
 }
+
