792a97d525054f29a1f047e6fbe985cda0b377cb
Albert Ye <albert.ye.728@gmail.com>
2020-07-14 17:28:56 -0700

    still unfinished

--- probs/codeforces/edu91/e.cpp
+++ probs/codeforces/edu91/e.cpp
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-int n, m, t[100005], dep[100005], par[100005][20];
-vector<int> adj[100005];
-bool vis[100005];
+int n, m, t[200005], val[200005], dep[200005], par[200005][20];
+vector<int> adj[400005];
+bool vis[200005];
 
 // finds the depths
 void dfs(int v, int p)
 {
-	if(p >= 0)
+	dep[v] = 1 + dep[p];
+	if(p > 0)
 	{
-		dep[v] = 1 + dep[p];
 		par[v][0] = p;
 	}
 	for(int x : adj[v])
@@ -27,9 +27,9 @@ void precomp()
 	// cerr << "ok" << endl;
 	for(int j = 1; j < 20; j++)
 	{
-		for(int i = 0; i < n; i++)
+		for(int i = 1; i <= m; i++)
 		{
-			if(par[i][j-1] != -1)
+			if(par[i][j-1] > 0)
 			{
 				par[i][j] = par[par[i][j-1]][j-1];
 			}
@@ -45,13 +45,13 @@ int lca(int u, int v)
 
 	for(int i = 0; i < 20; i++)
 	{
-		if(x >> i & 1) v = par[v][i];
+		if((x >> i) & 1) v = par[v][i];
 	}
 	if(u == v) return u;
 
 	for(int i = 19; i >= 0; i--)
 	{
-		if(par[u][i] != par[v][i])
+		if(par[u][i] != par[v][i] && par[u][i] > 0 && par[v][i] > 0)
 		{
 			u = par[u][i];
 			v = par[v][i];
@@ -60,8 +60,44 @@ int lca(int u, int v)
 	return par[u][0];
 }
 
+int when[200005], qry[200005][2];
+int ct = m;
 int main()
 {
-	int n;
-	cin >> n;
+	cin >> n >> m;
+	ct = m;
+	for(int i = 1; i <= n; i++) cin >> t[i];
+	for(int i = 1; i <= m; i++) val[i] = i;
+	for(int i = 1; i <  m; i++) 
+	{
+		int a, b, oa, ob;
+		cin >> a >> b;
+		oa = a; ob = b;
+		qry[i][0] = oa;
+		qry[i][1] = ob;
+		a = val[a]; b = val[b];
+		ct++;
+		adj[a].push_back(ct);
+		adj[ct].push_back(a);
+		adj[b].push_back(ct);
+		adj[ct].push_back(b);
+		val[oa] = ct;
+	}
+	par[ct][0] = ct;
+	dfs(ct, ct);
+	precomp();
+	int tot = 0;
+	for(int i = 1; i <  n; i++)
+	{
+		int l = lca(t[i],t[i+1])-m; 
+		if(l <= 0) continue;
+		when[l]++;
+		tot++;
+	}
+	cout << tot << endl;
+	for(int i = 1; i <  m; i++)
+	{
+		tot -= when[i];
+		cout << tot << endl;
+	}
 }
