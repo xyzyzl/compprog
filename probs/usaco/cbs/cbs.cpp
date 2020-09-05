3d8c5cdcdbabe84d469b18a2d3dca8161aec0833
Albert Ye <albert.ye.728@gmail.com>
2020-09-04 18:53:07 -0700

    cbs not solve, 1396C solved

--- probs/usaco/cbs/cbs.cpp
+++ probs/usaco/cbs/cbs.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int k,n,pr[15][50005],ps[15][50005];
+int k,n,pr[15][50005],ps[15][50005],ct[15][50005];
 int main()
 {
 	cin >> k >> n;
@@ -24,12 +24,13 @@ int main()
 		{
 			ps[i][j] = ps[i][j-1] + ps[i][j];
 		}
-		stack<int> mono;
+		stack<int> mono, mon2;
 		mono.push(0);
 		pr[i][0] = -1;
 		for(int j = 1; j < n; j++)
 		{
 			while(!mono.empty() && ps[i][mono.top()] >= ps[i][j]) mono.pop();
+			while(!mon2.empty() && ps[i][mon2.top()] >  ps[i][j]) mon2.pop();
 			if(mono.empty())
 			{
 				pr[i][j] = -1;
