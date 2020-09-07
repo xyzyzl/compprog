72679a212c80750d3cdd7a24ca914f1864698f4c
Albert Ye <albert.ye.728@gmail.com>
2020-09-07 11:10:20 -0700

    help

--- probs/usaco/help/help.cpp
+++ probs/usaco/help/help.cpp
@@ -1,67 +1,11 @@
-// Time:
-// Algorithms: 
-
-#include <bits/stdc++.h>
-
-#pragma GCC optimize("O3")
+#include <iostream>
 
 using namespace std;
 
-#define MAXN 1000005
-
-#define FOR(i, n) for (int i = 0; i < n; i++)
-#define FORR(j, i, n) for (int i = j; i < n; i++)
-#define FORD(i, n) for (int i = n - 1; i >= 0; i--)
-#define FOBIR(i, n) for (int i = 1; i <= n; i++)
-#define FOBIRD(i, n) for (int i = n; i >= 1; i--)
-#define pb push_back
-#define mp make_pair
-#define endl '\n'
-#define DUEHOANG                  \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(NULL);                \
-    cout.tie(NULL)
-
-#define fileio(in, out)      \
-    freopen(in, "r", stdin); \
-    freopen(out, "w", stdout);
-
 #define ll long long
-#define INF (1e9*1)+5
-
-typedef set<int> si;
-typedef vector<int> vi;
-typedef pair<int, int> pii;
-typedef pair<int, pii> iii;
-typedef vector<pii> vii;
-typedef vector<iii> viii;
-typedef priority_queue<int> pqi;
-typedef stack<int> sti;
-typedef queue<int> qi;
-typedef deque<int> di;
-typedef map<int, int> mii;
-#define f first
-#define s second
-
-#define LEFT(x) 2 * x
-#define RIGHT(x) 2 * x + 1
-
-#define bitinc(x) x&-x
-
-const int MAX(int &a, int b)
-{
-    return a = max(a, b); 
-}
-const int MIN(int &a, int b)
-{
-    return a = min(a, b); 
-}
-
-#define int ll
 #define MOD 1e9+7
 
 // due to Benjamin Qi
-// this I believe is now deleted; last year I was informed by a classmate about this snippet
 
 namespace modOp
 {
@@ -83,34 +27,34 @@ namespace modOp
     }
 }
  
-using namespace modOp; // benq is lying when saying that no one can help me
-// because he just helped me a great deal :blobheart:
+using namespace modOp;
 
-int n, e[MAXN*2], d[MAXN*2], twotothe[MAXN*2];
-pii ln[MAXN];
-signed main()
+int n, p[200005];
+int main()
 {
-    fileio("help.in", "help.out");
-    int t = 1;
-    // cin >> t; // uncomment if it's multitest
-    while(t--)
-    {
-        cin >> n;
-        FOR(i, n)
-        {
-            cin >> ln[i].f >> ln[i].s;
-            e[ln[i].f] = 1;
-            e[ln[i].s] = -1;
-        }
-        FOBIR(i, 2*n) e[i] += e[i-1];
-        // sweepline
-        // FOR(i, n) cerr << e[i] << endl;
-        int res = 0;
-        FOR(i, n)
-        {
-			AD(res, po(2, n-1-e[ln[i].f-1]));
-        }
-        cout << res << endl;
-    }
-    
+	freopen("help.in", "r", stdin);
+	freopen("help.out", "w", stdout);
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n;
+	for(int i = 0; i < n; i++)
+	{
+		int l,r;
+		cin >> l >> r;
+		p[l]++;
+		p[r]--;
+	}
+	// all l,r are distinct
+	// so line begins whenever 1 is found and ends whenever -1 is found
+	int cur = 0, ans = 0;
+	for(int i = 0; i <= 2*n; i++)
+	{
+		if(p[i]>0) 
+		{
+			AD(ans, po(2, n-1-cur));
+		}
+		cur += p[i];
+	}
+	cout << ans << endl;
 }
