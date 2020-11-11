156cdae6625cc9048e06e7373bdbd680bbffb68c
Albert Ye <albert.ye.728@gmail.com>
2020-11-10 18:17:37 -0800

    wow

--- probs/cf/round349/d.cpp
+++ probs/cf/round349/d.cpp
@@ -1,113 +1,138 @@
-
 #include <bits/stdc++.h>
 #include <bits/extc++.h>
 
 using namespace std;
 
-int n, m, vs[3005], dst[3005], ml[3005], mr[3005];
-vector<int> l[3005], r[3005];
-vector<int> adj[3005], adj2[3005];
+int n, m, vs[3005], dst[3005], ml[3005][3], mr[3005][3], D[3005][3005], RD[3005][3005];
+int _D[3005][3005], _RD[3005][3005];
+vector<int> l[3005], r[3005], adj[3005], adj2[3005];
 int main()
 {
-    cin >> n >> m;
-    for(int i = 0; i < m; i++)
-    {
-        int a, b;
-        cin >> a >> b;
-        --a; --b;
-        if(a==b) continue;
-        adj[a].push_back(b);
-        adj2[b].push_back(a);
-    }
-    for(int i = 0; i < n; i++)
-    {
-        memset(dst, 0x7f, sizeof dst);
-        memset(vs, 0, sizeof vs);
-        dst[i] = 0;
-        queue<int> q;
-        q.push(i);
-        while(!q.empty())
-        {
-            int x = q.front();
-            q.pop();
-            if(vs[x]) continue;
-            vs[x] = 1;
-            for(int y : adj[x])
-            {
-                if(vs[y]) continue;
-                dst[y] = dst[x] + 1;
-                q.push(y);
-            }
-        }
-        for(int j = 0; j < n; j++)
-        {
-            if(dst[j] > 2e9) continue;
-            if(dst[j] > ml[i])
-            {
-                l[i].clear();
-                l[i].push_back(j);
-            } else if(dst[j] == ml[i])
-            {
-                l[i].push_back(j);
-            }
-            ml[i] = max(ml[i], dst[j]);
-        }
-    }
-    for(int i = 0; i < n; i++)
-    {
-        memset(dst, 0x7f, sizeof dst);
-        memset(vs, 0, sizeof vs);
-        dst[i] = 0;
-        queue<int> q;
-        q.push(i);
-        while(!q.empty())
-        {
-            int x = q.front();
-            q.pop();
-            if(vs[x]) continue;
-            vs[x] = 1;
-            for(int y : adj2[x])
-            {
-                if(vs[y]) continue;
-                dst[y] = dst[x] + 1;
-                q.push(y);
-            }
-        }
-        for(int j = 0; j < n; j++)
-        {
-            if(dst[j] > 2e9) continue;
-            if(dst[j] > mr[i])
-            {
-                r[i].clear();
-                r[i].push_back(j);
-            } else if(dst[j] == mr[i])
-            {
-                r[i].push_back(j);
-            }
-            mr[i] = max(mr[i], dst[j]);
-        }
-    }
-    /*
+	cin >> n >> m;
+	for (int i = 0; i < m; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		--a;
+		--b;
+		if (a == b)
+			continue;
+		adj[a].push_back(b);
+		adj2[b].push_back(a);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		memset(dst, -0x7f, sizeof dst);
+		memset(vs, 0, sizeof vs);
+		dst[i] = 0;
+		queue<int> q;
+		q.push(i);
+		while (!q.empty())
+		{
+			int x = q.front();
+			q.pop();
+			if (vs[x])
+				continue;
+			vs[x] = 1;
+			for (int y : adj[x]) if(dst[y] < 0)
+			{
+				dst[y] = dst[x] + 1;
+				q.push(y);
+			}
+		}
+		for (int j = 0; j < n; j++)
+		{
+			if (dst[j] > 2e9)
+			{
+				D[i][j] = -1;
+				continue;
+			}
+			D[i][j] = dst[j];
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		memset(dst, -0x7f, sizeof dst);
+		memset(vs, 0, sizeof vs);
+		dst[i] = 0;
+		queue<int> q;
+		q.push(i);
+		while (!q.empty())
+		{
+			int x = q.front();
+			q.pop();
+			if (vs[x])
+				continue;
+			vs[x] = 1;
+			for (int y : adj2[x]) if(dst[y] < 0)
+			{
+				dst[y] = dst[x] + 1;
+				q.push(y);
+			}
+		}
+		for (int j = 0; j < n; j++)
+		{
+			if (dst[j] > 2e9)
+			{
+				RD[i][j] = -1;
+				continue;
+			}
+			RD[i][j] = dst[j];
+		}
+	}
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			_D[i][j] = D[i][j];
+			_RD[i][j] = RD[i][j];
+		}
+	}
+
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < 3; j++)
+		{
+			int k = max_element(D[i], D[i]+n)-D[i];
+			r[i].push_back(k);
+			D[i][k] = -1;
+			k = max_element(RD[i], RD[i]+n)-RD[i];
+			l[i].push_back(k);
+			RD[i][k] = -1;
+		}
+	}
+
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n; j++)
+		{
+			D[i][j] = _D[i][j];
+			RD[i][j] = _RD[i][j];
+		}
+	}
+	/*
     for(int i = 0; i < n; i++) for(int L : l[i]) for(int R : r[i])
     {
         cerr << L << ' ' << i << ' ' << R << endl;
     }
     */
-    int mx = 0;
-    int a=0,b=0,c=0,d=0;
-    for(int i = 0; i < n; i++) for(int L : l[i]) for(int M : l[L]) for(int R : r[i])
-    {
-        if(L == R || L == i || R == i || L == M || i == M || R == M) continue;
-        int x = mr[i] + ml[i] + ml[L];
-        if(x > mx)
-        {
-            mx = x;
-            a=R+1;
-            b=i+1;
-            c=L+1;
-            d=M+1;
-        }
-        // cerr << x << ' ' << r[i] << ' ' << i << ' ' << l[i] << ' ' << l[l[i]] << endl;
-    }
-    cout << a << ' ' << b << ' ' << c << ' ' << d << endl;
+	int mx = 0;
+	int a = 0, b = 0, c = 0, d = 0;
+	for(int i = 0; i < n; i++) for(int j = 0; j < n; j++) if(i != j)
+	{
+		for(auto L : l[i]) for(auto R : r[j]) if(L != R && L != i && R != i && L != j && R != j)
+		{
+			int x = RD[i][L] + D[i][j] + D[j][R];
+			if(x > mx)
+			{
+				// cerr << x << endl;
+				mx = x;
+				a = L+1;
+				b = i+1;
+				c = j+1;
+				d = R+1;
+			}
+		}
+	}
+	cout << a << ' ' << b << ' ' << c << ' ' << d << endl;
 }
